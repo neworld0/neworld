f246291387b21484aa66a14e4a2bb8476a540f1c
Peter JH. Lee <neworld0@gmail.com>
2021-08-27 19:45:27 +0900

    ALLOWED_HOSTS modified

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.37.168.232']
+ALLOWED_HOSTS = ['3.37.168.232', '321son.neworld.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
