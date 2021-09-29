e6c41a5567ac65bfa71f53f00908cbdf1980f7e8
Peter JH. Lee <neworld0@gmail.com>
2021-09-29 19:00:03 +0900

    user test 원상 복구 version

--- common/forms.py
+++ common/forms.py
@@ -8,3 +8,28 @@ class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "first_name", "last_name", "password1", "password2", "email")
+
+
+# from django.contrib.auth.forms import UserCreationForm
+# from django.core.validators import validate_email
+# from common.models import User
+#
+#
+# class UserForm(UserCreationForm):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.fields['username'].validators = [validate_email]
+#         self.fields['username'].help_text = '이메일 형식을 입력하세요.'
+#         self.fields['username'].label = 'Email'
+#
+#     def save(self, commit=True):
+#         user = super().save(commit=False)
+#         user.email = user.username
+#         if commit:
+#             user.save()
+#         return user
+#
+#     class Meta(UserCreationForm.Meta):
+#         model = User
+
+
--- common/urls.py
+++ common/urls.py
@@ -10,4 +10,13 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('profile/', views.profile, name='profile'),
     path('password_change/', views.MyPasswordChangeView.as_view(), name='password_change'),
+    path('password_reset/', views.MyPasswordResetView.as_view(), name='password_reset'),
+    path('reset/<uidb64>/<token>/', views.MyPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
+
+
+
+# path('login/url/', views.RequestLoginViaUrlView.as_view(template_name='common/request_login_via_url_form.html'), name='request_login_via_url'),
+# path('login/<uidb64>/<token>/', views.login_via_url, name='login_via_url'),
+
+# path('activate/<uidb64>/<token>/', views.activate, name='activate'),
