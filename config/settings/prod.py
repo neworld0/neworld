013ee637519dab6abec4ccded7e21df1a39af27e
neworld0 <ubuntu@ip-172-31-5-6.ap-northeast-2.compute.internal>
2021-10-02 15:52:24 +0900

    from EC2

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.37.168.232', '321son.neworld.kr']
+ALLOWED_HOSTS = ['3.37.207.152', 'www.neworld.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
