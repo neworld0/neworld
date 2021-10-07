09e17bf26a46fe31e347648bd48af92e5b0f1518
Peter JH. Lee <neworld0@gmail.com>
2021-10-07 17:01:21 +0900

    prod.py modified

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,7 +3,7 @@ from .base import *
 ALLOWED_HOSTS = ['3.37.207.152', 'www.neworld.kr', 'neworld.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
-DEBUG = False
+DEBUG = True
 
 STATICFILES_DIRS = [
     '/projects/mysite/static/img/',
