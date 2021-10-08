1aa64fc834a5b93e337e1ba37a0d5177ac002584
Peter JH. Lee <neworld0@gmail.com>
2021-10-08 23:54:12 +0900

    free tier applied

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -14,8 +14,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dbcamp',
         'USER': 'dbmaster',
-        'PASSWORD': 'RM9qBQCVzeBgNNxDIUJQ',
-        'HOST': 'database-1.cjo3f5o7oksd.ap-northeast-2.rds.amazonaws.com',
+        'PASSWORD': 'WTmZdaBclaNrvZVLcfol',
+        'HOST': 'databasecamp.cjo3f5o7oksd.ap-northeast-2.rds.amazonaws.com',
         'PORT': '5432',
     }
 }
