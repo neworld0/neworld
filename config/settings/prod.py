aa1d3f823b36ace9a92067a364c50e3cc3847161
unknown <neworld0@gmail.com>
2022-08-19 22:14:24 +0900

    prod.py changed 8/19 22:14

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,4 +2,10 @@ from .base import *
 
 ALLOWED_HOSTS = ['3.39.12.91']
 
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = []
+DEBUG = False
 
+STATICFILES_DIRS = [
+    '/projects/mysite/static/img/',
+]
