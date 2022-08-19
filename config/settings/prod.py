12a7dd1ad211dd389b646b1c6db23264d1493875
unknown <neworld0@gmail.com>
2022-08-19 21:03:45 +0900

    8/19 21:03

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -14,13 +14,6 @@ env = environ.Env()
 environ.Env.read_env(BASE_DIR / '.env')
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': '5432',
-    }
+
 }
 
