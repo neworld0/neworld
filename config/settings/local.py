2201bcd7a0a7ef67a9e3dd6dd5ab65912a1d1248
Peter JH. Lee <neworld0@gmail.com>
2022-08-19 16:50:34 +0900

    8/19 16:50

--- config/settings/local.py
+++ config/settings/local.py
@@ -6,14 +6,3 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
     '/projects/mysite/static/img/',
 ]
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': '2tjdudEgjs!',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
