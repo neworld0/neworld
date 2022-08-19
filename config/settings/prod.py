7f06c61eb7936055691aa0f302c178b9dbf60c6b
Peter JH. Lee <neworld0@gmail.com>
2022-08-19 15:55:57 +0900

    8/19 15:55

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.37.207.152', 'www.neworld.kr', 'neworld.kr']
+ALLOWED_HOSTS = ['3.39.12.91', 'www.neworld.kr', 'neworld.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
@@ -8,14 +8,14 @@ DEBUG = False
 STATICFILES_DIRS = [
     '/projects/mysite/static/img/',
 ]
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'neworld',
-        'USER': 'dbmaster',
-        'PASSWORD': 'WTmZdaBclaNrvZVLcfol',
-        'HOST': 'databasecamp.cjo3f5o7oksd.ap-northeast-2.rds.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'neworld',
+#         'USER': 'dbmaster',
+#         'PASSWORD': 'WTmZdaBclaNrvZVLcfol',
+#         'HOST': 'databasecamp.cjo3f5o7oksd.ap-northeast-2.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
