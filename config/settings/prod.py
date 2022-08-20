2a3cffa5a0522ac527647f1e1e6b41f97dcfe8ea
unknown <neworld0@gmail.com>
2022-08-20 15:29:16 +0900

    base.py changed 8/20 15:29

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.39.12.91']
+ALLOWED_HOSTS = ['3.39.12.91', 'neworld.kr', 'www.neworld.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
