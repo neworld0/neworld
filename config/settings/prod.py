93b6313e2384c4e53125ea706359a47f04a49244
unknown <neworld0@gmail.com>
2022-08-20 12:40:53 +0900

    base.py changed 8/20 12:40

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,5 +1,10 @@
 from .base import *
+import os
 
 ALLOWED_HOSTS = ['3.39.12.91']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
