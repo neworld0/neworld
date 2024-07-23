00b9e550c999eee1b2ebefc5d2c58832f6ba3a95
Peter JH. Lee <neworld0@gmail.com>
2024-07-23 14:03:03 +0900

    20240723 debug

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -5,7 +5,7 @@ from .base import *
 ALLOWED_HOSTS = ['3.39.12.91', 'neworld.kr', 'www.neworld.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
-DEBUG = False
+DEBUG = True
 
 env = environ.Env()
 environ.Env.read_env(BASE_DIR / '.env')
